91e853eb87913b86d77d20d4a831a89d59da4693
Ola Edin <olaedin@gmail.com>
2021-01-08 17:56:39 +0100

    Lesson 18

--- cheffy/src/app/auth/events.cljs
+++ cheffy/src/app/auth/events.cljs
@@ -1,5 +1,5 @@
 (ns app.auth.events
-  (:require [re-frame.core :refer [reg-event-fx]]))
+  (:require [re-frame.core :refer [reg-event-db reg-event-fx]]))
 
 (reg-event-fx
   :log-in
@@ -28,3 +28,24 @@
                                              :saved   #{}
                                              :inboxes {}}))
      :dispatch [:active-nav :saved]}))
+
+(reg-event-fx
+  :log-out
+  (fn [{:keys [db]} [_ _]]
+    {:db       (assoc-in db [:auth :uid] nil)
+     :dispatch [:active-nav :recipes]}))
+
+(reg-event-db
+  :update-profile
+  (fn [db [_ profile]]
+    (let [uid (get-in db [:auth :uid])]
+      (update-in db [:users uid :profile] merge (select-keys profile [:first-name :last-name])))))
+
+(reg-event-fx
+  :delete-profile
+  (fn [{:keys [db]} _]
+    (let [uid (get-in db [:auth :uid])]
+      {:db       (-> db
+                     (assoc-in [:auth :uid] nil)
+                     (update-in [:users] dissoc uid))
+       :dispatch [:active-nav :recipes]})))
--- cheffy/src/app/auth/views/profile.cljs
+++ cheffy/src/app/auth/views/profile.cljs
@@ -1,6 +1,52 @@
 (ns app.auth.views.profile
-  (:require [app.components.page-nav :refer [page-nav]]))
+  (:require [re-frame.core :as rf]
+            [reagent.core :as r]
+            ["@smooth-ui/core-sc" :refer [Row Col Box Button Typography]]
+            [app.components.form-group :refer [form-group]]
+            [app.components.page-nav :refer [page-nav]]))
 
 (defn profile
   []
-  [page-nav {:center "profile"}])
+  (let [{:keys [first-name last-name]} @(rf/subscribe [:active-user-profile])
+        initial-values {:first-name first-name
+                        :last-name  last-name}
+        values         (r/atom initial-values)]
+    [:<>
+     [page-nav {:center "profile"
+                :right  [:> Button {:variant  "light"
+                                    :on-click #(rf/dispatch [:log-out])} "Log Out"]}]
+     [:> Row {:justify-content "center"}
+      [:> Col {:xs 12 :sm 6}
+       [:> Box {:background-color "white"
+                :border-radius    10
+                :p                3
+                :pt               1}
+        [:> Typography {:variant     "h4"
+                        :py          10
+                        :font-weight 700}
+         "Personal Info"]
+        [form-group {:id     :first-name
+                     :label  "First Name"
+                     :type   "text"
+                     :values values}]
+        [form-group {:id     :last-name
+                     :label  "Last Name"
+                     :type   "text"
+                     :values values}]
+        [:> Box {:display         "flex"
+                 :justify-content "flex-end"}
+         [:> Button {:on-click #(rf/dispatch [:update-profile @values])} "Update Profile"]]]
+       [:> Box {:background-color "white"
+                :border-radius    10
+                :p                3
+                :pt               1
+                :mt               40}
+        [:> Typography {:variant     "h4"
+                        :py          10
+                        :font-weight 700}
+         "Danger Zone"]
+        [:> Box {:display         "flex"
+                 :justify-content "flex-end"}
+         [:> Button {:variant  "danger"
+                     :on-click #(when (js/confirm (str "Do you want to delete the account \"" first-name " " last-name "\"?"))
+                                  (rf/dispatch [:delete-profile @values]))} "Delete Profile"]]]]]]))
